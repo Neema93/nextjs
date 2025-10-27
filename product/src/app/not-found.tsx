19dadbe9cb63e3ce2aa902170592d38a380dfc93
Neema <patelneema93@gmail.com>
2025-10-27 11:26:55 -0400

    change header

--- product/src/app/not-found.tsx
+++ product/src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default  function NotFound() {
     return(
         <>
         <h1>Page Not Found</h1>
-        <h3>could not find request resource</h3>
+        <h4>could not find request resource</h4>
         </>
     )
 }
